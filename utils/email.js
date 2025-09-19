605ef637682b1dce11d9d96b8a3b70a2b2ddef4e
yash tiwari <yashtiwari700714@gmail.com>
2025-09-20 00:39:29 +0530

    refactor

--- utils/email.js
+++ utils/email.js
@@ -1,6 +1,7 @@
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const api = process.env.RESEND_API_KEY;
+const resend = new Resend(api);
 
 export async function sendEmail({ to, subject, html }) {
     try {
